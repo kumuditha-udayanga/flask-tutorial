219d3daf7a6b732b9341affd94d05db4631f2ff4
Kumuditha Karunarathna <kumudithaudaya@gmail.com>
2019-12-18 13:10:13 +0530

    Modify routes

--- application.py
+++ application.py
@@ -10,19 +10,23 @@ def hello_world():
 @app.route("/user/<name>")
 def display_user(name):
     print(name)
+    return name
 
 
 @app.route("/total/<int:amount>")
 def display_total_amount(amount):
     print(amount)
+    return "Amount is " + str(amount)
 
 
 @app.route("/path/<path:sub_path>")
 def take_to_subpath(sub_path):
 
     print(sub_path)
+    return sub_path
 
 
 @app.route("/key/<uuid:api_key>")
 def display_key(api_key):
     print(api_key)
+    return "done"

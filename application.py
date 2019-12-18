78704045e025226907170739fa274c6d65abacfb
Kumuditha Karunarathna <kumudithaudaya@gmail.com>
2019-12-18 12:57:23 +0530

    Add routes

--- application.py
+++ application.py
@@ -5,3 +5,24 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+
+@app.route("/user/<name>")
+def display_user(name):
+    print(name)
+
+
+@app.route("/total/<int:amount>")
+def display_total_amount(amount):
+    print(amount)
+
+
+@app.route("/path/<path:sub_path>")
+def take_to_subpath(sub_path):
+
+    print(sub_path)
+
+
+@app.route("/key/<uuid:api_key>")
+def display_key(api_key):
+    print(api_key)
